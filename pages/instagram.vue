a2e82a41a0b54f35b050acbc289aa5285146764f
Gabrielfernandes87f <gabriel1coder@gmail.com>
2022-10-14 03:28:34 -0300

    mudando interaçoes de texto gravar

--- pages/instagram.vue
+++ pages/instagram.vue
@@ -1020,7 +1020,7 @@
               <input type="checkbox" id="my-modal-6" class="modal-toggle" />
               <label for="my-modal-6" class="modal cursor-pointer">
                 <label class="modal-box relative" for="">
-                  <h3 class="text-lg font-bold">Filmar</h3>
+                  <h3 class="text-lg font-bold">Você está sendo filmado</h3>
                   <p class="py-4"></p>
                 </label>
               </label>
